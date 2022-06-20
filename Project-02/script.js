f7fd4f0920fcd230fe3c7882a4d8af0f011e4228
fayyazbinfida <khanfayyaz45@gmail.com>
2022-06-20 22:43:00 +0500

    Created all JavaScript and Customise Index Page
    Created all Functions

---
+++ Project-02/script.js
@@ -0,0 +1,62 @@
+//Get DOM Elements
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat');
+ const count = document.getElementById('count');
+ const total = document.getElementById('total');
+ const selectMovie = document.getElementById('movie');
+
+ //call the UI function - get data from local storage and update the UI
+updateUI();
+
+ //Get the ticket price from the selectMovie Dropdown
+ let ticketPrice = +selectMovie.value;
+
+ //Function to update count
+ function updateCount() {
+     //calculate how many seats are selected
+     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+     //create an array using the node list
+     const seatIndex = [...selectedSeats].map( seat => [...seats].indexOf(seat) );
+     //Get the number of seats from the node list
+     const selectedSeatsCount = selectedSeats.length;
+     //update DOM with the count
+     count.innerText = selectedSeatsCount;
+     //update DOM with total Price
+     total.innerText = selectedSeatsCount * ticketPrice;
+     //save data to local storage
+     localStorage.setItem('selectedSeats',JSON.stringify(seatIndex));
+    }
+
+//Function to save the selected movie data in local storage
+    function saveMovieData(movieIndex, moviePrice) {
+        localStorage.setItem('movieIndex', movieIndex);
+        localStorage.setItem('moviePrice', moviePrice);
+    };
+
+//Function to get data from local storage and update the UI
+function updateUI() {
+    const selectedSeats = JASON.parse(localStorage.getItem('selectedSeats'));
+
+}
+
+//Event Listners
+//1. Listen for click on container
+container.addEventListener('click', e => {
+    //Check if target has a class od seat and also is not occupied
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied') )  {
+        //Add or remove the selected class on click
+        e.target.classList.toggle('selected');
+        //update the count of selected seats
+        updateCount();
+    }
+});
+
+//2. Listen for change in movie selection
+selectMovie.addEventListener('change', e => {
+    //update ticket price to the selected movie
+    ticketPrice = +e.target.value;
+    //update the counts in DOM
+    updateCount();
+    //save the movie data to local storage
+    saveMovieData(e.target.selectedIndex,e.target.value);
+})
