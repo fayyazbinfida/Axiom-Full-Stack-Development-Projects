c6ebc95f8ed6bc71b5a61e25e1f7d73705b72fb2
fayyazbinfida <khanfayyaz45@gmail.com>
2023-04-18 22:24:32 +0500

    index and css completed
    index and css completed , now working on js

---
+++ Project-03/script.js
@@ -0,0 +1,7 @@
+// Get DOM Element
+const Video = document.getElementById('video'); 
+const Play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const time = document.getElementById('time');
+
